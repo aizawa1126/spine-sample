8bd0e06ec605432d1042592aa54c32a2667d1083
Tatsuya Aizawa <Tatsuya.Aizawa@jrits.ricoh.co.jp>
2012-07-26 11:42:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpineSample::Application.config.secret_token = '6bc74365a45c982b64d9d276da7ddf0bc9bd01cffb00ce7370545bf0824cbec0979a411f5f273abd6c915a2e76ba6be855f2526a91c229932b5da5f11275ed4d'
